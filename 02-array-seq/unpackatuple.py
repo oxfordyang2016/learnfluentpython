551924a1c4b2eed2d04f122b32deebb7cb590ed1
yangming <yang756260386@gmail.com>
2017-02-23 16:30:37 +0800

    allok

--- 02-array-seq/unpackatuple.py
+++ 02-array-seq/unpackatuple.py
@@ -1,5 +1,12 @@
 #fp28
 #unpack a tuple 's advanced usage
+#simple
+a,b,c=(1,2,3)
+#related a list
+a,*b,c=(1,23,3,4,56,7,8,99,9)
+#b= [23, 3, 4, 56, 7, 8, 99] is a list
+
+
 def unpackatuple(a,b,c):
     return(a+b+c)
 #The % formatting operator understands tuples and treats each item as a separate field
@@ -43,4 +50,28 @@ but it can appear in any position:
 >>> head, b, c, d
 ([0, 1], 2, 3, 4)
 Finally, a powerful feature of tuple unpacking is that it works with nested structures
+'''
+
+
+'''
+unpack a nested tupe and print format contorl
+In [114]: metro_areas = [
+     ...:  ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)), #
+     ...:  ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+     ...:  ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+     ...:  ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+     ...:  ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),]
+
+
+In [115]: print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
+     ...: fmt = '{:15} | {:9.4f} | {:9.4f}'
+     ...: for name, cc, pop, (latitude, longitude) in metro_areas: #
+     ...:     if longitude <= 0: #
+     ...:         print(fmt.format(name, latitude, longitude))
+
+
+                |   lat.    |   long.
+Mexico City     |   19.4333 |  -99.1333
+New York-Newark |   40.8086 |  -74.0204
+Sao Paulo       |  -23.5478 |  -46.6358
 '''

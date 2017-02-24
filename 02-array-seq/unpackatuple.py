d0d4e41de6fd8abd6d1f83ed874628500d8b3c80
yangming <yang756260386@gmail.com>
2017-02-24 16:08:51 +0800

    allok

--- 02-array-seq/unpackatuple.py
+++ 02-array-seq/unpackatuple.py
@@ -75,3 +75,124 @@ Mexico City     |   19.4333 |  -99.1333
 New York-Newark |   40.8086 |  -74.0204
 Sao Paulo       |  -23.5478 |  -46.6358
 '''
+
+
+
+# namedtuple fp 30 how to think the use of named tuple
+#method 1
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+#method 2
+'''
+Example 2-9. Defining and using a named tuple type
+>>> from collections import namedtuple
+>>> City = namedtuple('City', 'name country population coordinates')
+>>> tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+>>> tokyo
+City(name='Tokyo', country='JP', population=36.933, coordinates=(35.689722, 139.691667))
+>>> tokyo.population
+36.933
+>>> tokyo.coordinates
+(35.689722, 139.691667)
+>>> to
+Two parameters are required to create a named tuple: a class name and a list of
+field names, which can be given as an iterable of strings or as a single spacedelimited
+string.
+Data must be passed as positional arguments to the constructor (in contrast, the
+tuple constructor takes a single iterable).
+You can access the fields by name or position.
+'''
+'''
+#namedtuple as records.
+Example 2-10. Named tuple attributes and methods (continued from Example 2-9)
+>>> City._fields
+('name', 'country', 'population', 'coordinates')
+>>> LatLong = namedtuple('LatLong', 'lat long')
+>>> delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+>>> delhi = City._make(delhi_data)
+>>> delhi._asdict()
+OrderedDict([('name', 'Delhi NCR'), ('country', 'IN'), ('population',
+21.935), ('coordinates', LatLong(lat=28.613889, long=77.208889))])
+>>> for key, value in delhi._asdict().items():
+        print(key + ':', value)
+name: Delhi NCR
+country: IN
+population: 21.935
+coordinates: LatLong(lat=28.613889, long=77.208889)
+_fields is a tuple with the field names of the class.
+_make() lets you instantiate a named tuple from an iterable; City(*delhi_da
+ta) would do the same.
+_asdict() returns a collections.OrderedDict built from the named tuple
+instance. That can be used to produce a nice display of city data
+'''
+
+#slicing
+'''
+Every Python programmer knows that sequences can be sliced using the s[a:b] syntax.
+We now turn to some less well-known facts about slicing.
+
+'''
+
+#how to split  pagraph to lines????
+invoice = """
+0.....6.................................40........52...55........
+1909  Pimoroni PiBrella                      $17.50  3    $52.50
+1489  6mm Tactile Switchx20                   $4.95  2    $9.90
+1510  Panavise Jr. - PV-201                  $28.00  1    $28.00
+1601  PiTFT Mini Kit 320x240                 $34.95  1    $34.95
+ """
+
+line_items = invoice.split('\n')[3:]
+print(line_items)
+'''
+['1489  6mm Tactile Switchx20                   $4.95  2    $9.90',
+ '1510  Panavise Jr. - PV-201                  $28.00  1    $28.00',
+ '1601  PiTFT Mini Kit 320x240                 $34.95  1    $34.95',
+ ' ']
+'''
+
+invoicetest = """
+1909  Pimoroni PiBrella                      $17.50  3    $52.50
+1489  6mm Tactile Switchx20                   $4.95  2    $9.90
+1510  Panavise Jr. - PV-201                  $28.00  1    $28.00
+1601  PiTFT Mini Kit 320x240                 $34.95  1    $34.95
+ """
+print(invoicetest.split('\n'))
+'''
+['',
+ '1909  Pimoroni PiBrella                      $17.50  3    $52.50',
+ '1489  6mm Tactile Switchx20                   $4.95  2    $9.90',
+ '1510  Panavise Jr. - PV-201                  $28.00  1    $28.00',
+ '1601  PiTFT Mini Kit 320x240                 $34.95  1    $34.95',
+ ' ']
+'''
+ a=invoicetest.split('\n')#note that a[0]='',a[-1]='' for invoice test spicial first line last line format
+ print(a[1:len(a)-1])
+ '''
+ ['1909  Pimoroni PiBrella                      $17.50  3    $52.50',
+ '1489  6mm Tactile Switchx20                   $4.95  2    $9.90',
+ '1510  Panavise Jr. - PV-201                  $28.00  1    $28.00',
+ '1601  PiTFT Mini Kit 320x240                 $34.95  1    $34.95']
+  '''
+'''
+ In [45]: for k in a[1:len(a)-1]:
+    ...:     print(k[slice(0,2)])
+19
+14
+15
+16 
+'''
+'''
+SKU = slice(0, 6)
+DESCRIPTION = slice(6, 40)
+
+>>> UNIT_PRICE = slice(40, 52)#use this mwthod ,only for get a string certain position refer to the above
+>>> QUANTITY = slice(52, 55)
+>>> ITEM_TOTAL = slice(55, None)
+>>> line_items = invoice.split('\n')[2:]
+>>> for item in line_items:
+        print(item[UNIT_PRICE], item[DESCRIPTION])
+ $17.50 Pimoroni PiBrella
+ $4.95 6mm Tactile Switch x20
+ $28.00 Panavise Jr. - PV-201
+ $34.95 PiTFT Mini Kit 320x240
+ '''
